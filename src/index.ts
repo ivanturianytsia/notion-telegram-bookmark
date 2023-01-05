cd78213af8a39e2cee693a524fd6cc4fa375c1cb
Ivan Turianytsia <vanyaturianitsa@gmail.com>
2023-01-05 18:52:19 +0100

    Add express server

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import { Telegraf } from 'telegraf'
+import express from 'express'
+import { config } from 'dotenv'
 import { handleText } from './handler'
 
-import { config } from 'dotenv'
-
 config()
+
+// Telegram bot init
 
 if (!process.env.BOT_TOKEN) {
   throw new Error('BOT_TOKEN is not set.')
@@ -19,3 +21,17 @@ process.once('SIGINT', () => bot.stop('SIGINT'))
 process.once('SIGTERM', () => bot.stop('SIGTERM'))
 
 console.log('Bot is ready!')
+
+// Express app init
+
+const port = process.env.PORT || 80
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('The bot is working.')
+})
+
+app.listen(port, () => {
+  console.log(`Express app listening on http://localhost:${port}`)
+})
